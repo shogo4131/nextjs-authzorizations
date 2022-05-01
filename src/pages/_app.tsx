553815ab89b4d780631dbfe598df67fa804b5b8a
shogo4131 <shougo08697118@gmail.com>
2022-05-01 17:31:39 +0900

    fix: next auth sample

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
-};
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
 
-export default App;
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light'
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
+}
